461a960ef4a3079ca08646067eb790a3b275c468
Simi <simion.alexgabriel@yahoo.com>
2018-11-10 13:22:39 +0200

    feat: Add search bar

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import { MDCDrawer } from '@material/drawer/index';
 import { MDCTopAppBar } from '@material/top-app-bar/index';
+import { MDCTextField } from '@material/textfield';
 import './searchGroups';
 
 if ('serviceWorker' in navigator) {
@@ -8,10 +9,25 @@ if ('serviceWorker' in navigator) {
 
 const drawer = new MDCDrawer(document.querySelector('.mdc-drawer'));
 const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
+const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
 
 topAppBar.listen('MDCTopAppBar:nav', () => {
     drawer.open = !drawer.open;
 });
+
+// Search bar
+const searchBar = document.querySelector('.search-bar');
+const searchButton = document.querySelector('#search-button');
+
+searchButton.addEventListener('click', function () {
+    searchBar.style = "visibility:visible";
+});
+
+document.querySelector('.mdc-drawer-app-content').addEventListener('click', function () {
+    searchBar.style = "visibility:hidden";
+});
+
+
 
 // document.querySelector('#search-group').addEventListener('click', function () {
 //     drawer.open = false;
