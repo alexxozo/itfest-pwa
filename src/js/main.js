f3b7fd7aeaf17ac954874ec548d3497e2209f738
Simi <simion.alexgabriel@yahoo.com>
2018-11-10 15:51:08 +0200

    feat: Added courses view

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import { MDCDrawer } from '@material/drawer/index';
 import { MDCTopAppBar } from '@material/top-app-bar/index';
 import { MDCTextField } from '@material/textfield';
+import { MDCRipple } from '@material/ripple';
+import { MDCTabBar } from '@material/tab-bar';
 import './searchGroups';
 
 if ('serviceWorker' in navigator) {
@@ -10,6 +12,12 @@ if ('serviceWorker' in navigator) {
 const drawer = new MDCDrawer(document.querySelector('.mdc-drawer'));
 const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
 const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
+const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
+
+document.querySelectorAll('.mdc-button, .mdc-icon-button').forEach(function (button) {
+    const ripple = new MDCRipple(button);
+    ripple.unbounded = true;
+});
 
 topAppBar.listen('MDCTopAppBar:nav', () => {
     drawer.open = !drawer.open;
@@ -27,6 +35,13 @@ searchButton.addEventListener('click', function () {
 document.querySelector('.mdc-drawer-app-content').addEventListener('click', function () {
     searchBar.classList.remove('active');
 });
+
+// Classes select
+const listItems = document.querySelectorAll('.mdc-list-item');
+listItems.forEach((item) => item.addEventListener('click', function () {
+    listItems.forEach((i) => i.classList.remove('active'));
+    item.classList.add('active');
+}))
 
 // document.querySelector('#search-group').addEventListener('click', function () {
 //     drawer.open = false;
