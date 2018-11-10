60938ecb8a83e4a16d58c288f264e83886281125
Adrian <adriandinca2008@yahoo.com>
2018-11-10 22:29:59 +0200

    feat: Add SW

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,1 +1,32 @@
+workbox.core.setCacheNameDetails({
+    prefix: 'amda',
+});
+
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+workbox.routing.registerRoute(
+    /^https:\/\/fonts\.googleapis\.com/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'google-fonts-stylesheets',
+    })
+);
+
+workbox.routing.registerRoute(
+    /^https:\/\/use\.fontawesome\.com/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'google-fonts-stylesheets',
+    })
+);
+
+workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|svg)$/,
+    workbox.strategies.cacheFirst({
+        cacheName: 'images',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 60,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+            }),
+        ],
+    })
+);
